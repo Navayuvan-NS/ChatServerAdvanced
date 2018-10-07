3994c8d5ec2163b36df2f2b7f0cbce336188a143
Navayuvan-NS <ns.navayuvan@gmail.com>
2018-10-07 14:13:16 +0530

    07.10.2018

---
+++ Chatclient.java
@@ -0,0 +1,97 @@
+import javax.swing.*;  
+import java.awt.event.*; 
+import java.io.*;
+import java.net.*;
+
+class Chatclient {
+
+	public static void main(String[] args) {
+		try{
+			MyclientT myclientt = new MyclientT();
+			myclientt.start();
+		}
+		catch(Exception e){
+			System.out.println("Oops....!! Sorry. Error occured in Thread");
+			System.exit(0);
+		}
+	}
+}
+
+class MyclientT extends Thread{
+
+	JLabel l1,l2;
+	JTextArea area,space;
+	JButton b;
+	DataInputStream din;
+	DataOutputStream dout;
+	String msgin="",msgout="";
+	MyclientT(){
+		JFrame f = new JFrame("Client");
+
+		l1 = new JLabel();
+		l1.setBounds(220,10,280,30);
+		l1.setText("Client");
+		f.add(l1);
+
+		area = new JTextArea();
+		area.setBounds(5,50,490,480);
+		area.setVisible(true);
+		area.setFont(area.getFont().deriveFont(20.0f));
+		area.setEditable(false);
+		f.add(area);	
+
+		b = new JButton("Send");
+		b.setBounds(200,600,100,40);
+		f.add(b);
+
+		space = new JTextArea();
+		space.setBounds(5,540,490,50);
+		space.setFont(space.getFont().deriveFont(25.0f));
+		f.add(space);
+
+		f.setSize(500,700);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+
+	public void run(){
+		try{
+			Socket s = new Socket("localhost",6666);
+			DataInputStream din = new DataInputStream(s.getInputStream());
+        	DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        	//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        while(!msgin.equals("end")){
+	              
+	               msgout = space.getText();
+	               //msgout = br.readLine();
+	               area.append("You: "+msgout+"\n"); 
+
+	               b.addActionListener(new ActionListener(){
+	               		public void actionPerformed(ActionEvent e){
+	               			call();
+	               		}
+	               });
+	               msgin = din.readUTF();
+	               //System.out.println(msgin);
+	               area.append("Server: "+msgin+"\n");            
+	           }
+		}
+		catch(Exception e){
+			//System.out.println("Oops...!!! Problem in starting Client Socket.");
+			space.setText("Oops...!!! Problem in starting Client Socket");
+			space.setEditable(false);
+			System.exit(0);
+		}
+	}
+
+	public void call(){
+		try{
+			area.append("You: "+msgout+"\n"); 
+		    dout.writeUTF(msgout);
+		}
+		catch(Exception e){
+			space.setText("Error");
+		}
+	}
+}
---
+++ Chatserver.java
@@ -0,0 +1,101 @@
+import javax.swing.*;  
+import java.awt.event.*;  
+import java.io.*;
+import java.net.*;
+
+class Chatserver {
+
+	public static void main(String[] args) {
+		try{
+			MyserverT myservert = new MyserverT();
+			myservert.start();
+		}
+		catch(Exception e){
+			System.out.println("Oops....!! Sorry. Error occured in Thread");
+			System.exit(0);
+		}
+	}
+}
+
+class MyserverT extends Thread{
+
+	JLabel l1,l2;
+	JTextArea area,space;
+	JButton b;
+	DataInputStream din;
+	DataOutputStream dout;
+	String msgin="",msgout="";
+	MyserverT(){
+		JFrame f = new JFrame("Server");
+
+		l1 = new JLabel();
+		l1.setBounds(220,10,280,30);
+		l1.setText("Server");
+		f.add(l1);
+
+		area = new JTextArea();
+		area.setBounds(5,50,490,480);
+		area.setVisible(true);
+		area.setFont(area.getFont().deriveFont(20.0f));
+		area.setEditable(false);
+		f.add(area);	
+
+		b = new JButton("Send");
+		b.setBounds(200,600,100,40);
+		f.add(b);
+
+		space = new JTextArea();
+		space.setBounds(5,540,490,50);
+		space.setFont(space.getFont().deriveFont(25.0f));
+		f.add(space);
+
+		f.setSize(500,700);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+
+	public void run(){
+		try{
+			ServerSocket ss = new ServerSocket(6666);
+            Socket s = ss.accept();
+            DataInputStream din = new DataInputStream(s.getInputStream());
+	        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+	        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        while(!msgin.equals("end")){
+	        	if(space.getText() != null){
+	               msgin = din.readUTF();
+	               //System.out.println(msgin);
+	               area.append("Client: "+msgin+"\n");
+	               //msgout = br.readLine();
+	               msgout = space.getText();
+	               b.addActionListener(new ActionListener(){
+	               		public void actionPerformed(ActionEvent e){
+	               			call();
+	               		}
+	               });
+	               //dout.writeUTF(msgout);
+	               dout.flush();
+	            }
+	        }
+	    }
+		catch(Exception e){
+			//System.out.println("Oops...!!! Problem in starting Server Socket");
+			space.setText("Oops...!!! Problem in starting Server Socket");
+			space.setEditable(false);
+			System.exit(0);
+		}
+	}
+
+	public void call(){
+		try{
+			area.append("Server: "+msgout+"\n");
+			dout.writeUTF(msgout);
+		}
+		catch(Exception e){
+			space.setText("Error");
+		}
+	}
+
+}
